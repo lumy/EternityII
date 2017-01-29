c30fc3cb1d12264b02ecd992265cf7d578970f2f
lumy <lumy@lumy.me>
2017-01-29 03:31:51 +0100

    cleaning main and using ind.get_population

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-
-from puzzle import create_subdir
+import ind
 
 os.environ['TCL_LIBRARY'] = "C:\\Python27\\tcl\\tcl8.5"
 os.environ['TK_LIBRARY'] = "C:\\Python27\\tcl\\tk8.5"
@@ -9,6 +8,11 @@ import config
 from runner import Runner
 
 
+def create_subdir(s):
+  try:
+    os.mkdir(s)
+  except Exception as e:
+    print e
 
 def _load_file(s):
   try:
@@ -17,51 +21,18 @@ def _load_file(s):
   except Exception:
     return None
 
-#   def if_then_else(input, output1, output2):
-#     return output1 if input else output2
-#
-#
-# pset = PrimitiveSetTyped("main", [bool, float], float)
-# pset.addPrimitive(operator.xor, [bool, bool], bool)
-# pset.addPrimitive(operator.mul, [float, float], float)
-# pset.addPrimitive(if_then_else, [bool, float, float], float)
-# pset.addTerminal(3.0, float)
-# pset.addTerminal(1, bool)
-# [ X Y W Z]
-# N W S E
-# 0 is a border
-
-def print_pop(pop, elem=None):
-  def print_row(row, _elem=None):
-    def print_ind(ind):
-      if ind.fitness.valid:
-        print "N[%s]W[%s]S[%s]E[%s]-Fitness[%s]" % (ind[0], ind[1], ind[2], ind[3], ind.fitness)
-      else:
-        print "N[%s]W[%s]S[%s]E[%s]-Fitness[%s]" % (ind[0], ind[1], ind[2], ind[3], None)
-
-    for elem in row:
-      if _elem > 0 or _elem == None:
-        print_ind(elem)
-      if _elem != None:
-        _elem -= 1
-    return _elem
-
-  temp = elem
-  for row in pop:
-    temp = print_row(row, _elem=temp)
-
 def load_population(pid):
+
   f = _load_file(config.population_file_saved)
-  if f != None:
-    return f
-  f = _load_file(config.population_file_base)
-  f = [[int(y) for y in x.split()] for x in f]
-  corner = [l for l in f if l.count(0) == 2]
-  border = [l for l in f if l.count(0) == 1]
-  inside = [l for l in f if l.count(0) == 0]
+  if f != None: # Todo Save a basic population and reload it in the Runner
+    raise NotImplemented
+
+  # Loading a basic Population with a runner
+  inds = ind.get_population()
+  corner = [i for i in inds if i[1].count(0) == 2]
+  border = [i for i in inds if i[1].count(0) == 1]
+  inside = [i for i in inds if i[1].count(0) == 0]
   return Runner((corner, border, inside), pid)
-
-
 
 
 def main():
