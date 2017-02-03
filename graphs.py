33f25d1c9a727ced864914ad24b13b89c817fcd6
lumy <lumy@lumy.me>
2017-02-03 02:41:50 +0100

    #4 Generating Graph for a given population and a graph for all population over the time with a type stats like min max or avg. (On both Stats group and ind)

--- graphs.py
+++ graphs.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 
-def generate_graph_weight_population(generation, fits_ind, fits_group, saved, show, path):
+def _generate_graph_weight_population(xlabel, value1, value2, saved, show, fullpath):
   """
      Generate a graph and save it.
    :param ngen:
    :return:
    """
   fig, ax1 = plt.subplots()
-  line1 = ax1.plot(range(0, 256), fits_ind, "b.", label="Individual Fitness")
-  ax1.set_xlabel("Population @ %s" % generation)
-  ax1.set_ylabel("Individual Fitness", color="b")
+  line1 = ax1.plot(value1[0], value1[1], "b.", label=value1[2])
+  ax1.set_xlabel(xlabel)
+  ax1.set_ylabel(value1[2], color="b")
   for tl in ax1.get_yticklabels():
     tl.set_color("b")
 
   ax2 = ax1.twinx()
-  line2 = ax2.plot(range(0, 256), fits_group, "r.", label="Groups Fitness")
-  ax2.set_ylabel("Group Fitness", color="r")
+  line2 = ax2.plot(value2[0], value2[1], "r.", label=value2[2])
+  ax2.set_ylabel(value2[2], color="r")
   for tl in ax2.get_yticklabels():
     tl.set_color("r")
   lns = line1 + line2
@@ -25,9 +25,35 @@ def generate_graph_weight_population(generation, fits_ind, fits_group, saved, sh
   if show:
     plt.show()
   if saved:
-    plt.savefig("%s/ind_group_gen%s.png" % (path, generation), bbox_inches='tight', dpi=100)
+    plt.savefig(fullpath, bbox_inches='tight', dpi=100)
 
-    # nrow = [0, None, -1.0, 1.0]
+def generate_graph_weight_population(generation, fits_ind, fits_group, saved, show, path):
+      """
+         Generate a graph and save it.
+         Except generation to be on int and the data to be a ind and group fitnesses of one population.
+       :param ngen:
+       :return:
+       """
+      xlabel = "Population @ %s" % generation
+      inds = (range(0, 256), fits_ind, "Individual Fitness")
+      groups = (range(0, 256), fits_group, "Groups Fitness")
+      _generate_graph_weight_population(xlabel, inds, groups, saved, show, "%s/ind_group_gen%s.png" % (path, generation))
+
+def generate_graph_all_data(generations, fits_ind, fits_group, saved, show, path, ftype):
+        """
+           Generate a graph and save it.
+           Except generation to be all the generation and fits_ind and fits_group to be all the min or max fits or avg.
+         :param ngen:
+         :return:
+         """
+        xlabel = "Generations"
+        inds = (generations, fits_ind, "Individual Fitness %s" % ftype)
+        groups = (generations, fits_group, "Groups Fitness %s" % ftype)
+        _generate_graph_weight_population(xlabel, inds, groups, saved, show,
+                                          "%s/all_%s_ind_group.png" % (path, ftype))
+
+
+        # nrow = [0, None, -1.0, 1.0]
   # # [-100, -75, -50, -25, 0, 25, 50, 75, 100]
   # # Temporary fix until we now exactly what we want to log
   # y = 0
--- stats.py
+++ stats.py
@@ -42,9 +42,8 @@ class Stats(object):
     :return:
     """
     record = self.stats.compile(population)
-    fitness_ind, fitness_group = zip(*[(x.fitness_ind.values, x.fitness_group.values) for x in population])
     self.logbook.record(generations=generation, mutated=n_mutated, mutation_percent=config.mutate_inpd,
-                        individual_fitnesses=fitness_ind, group_fitnesses=fitness_group, population=population, **record)
+                        population=population, **record)
     # I case we need to keep famous big scores.
     # self.famous.update(self.pop)
 
@@ -61,13 +60,20 @@ class Stats(object):
     else:
       pickle.dump(self.logbook, open("%s/logbook.pkl" % self.personal_path, "w"))
 
-  def generate_graph(self, gen, inds, groups, saved=True, show=False):
-    graphs.generate_graph_weight_population(gen, inds, groups, saved, show, self.personal_path )
-
   def generate_graph_per_generations(self, saved=True, show=False):
-    gen, all_inds, all_groups = self.logbook.select("generations", "individual_fitnesses", "group_fitnesses")
+    gen, all_inds, all_groups = self.logbook.select("generations", "individual_fitness", "group_fitness")
     for g, inds, groups in zip(gen, all_inds, all_groups):
-       self.generate_graph(g, inds, groups, saved=saved, show=show)
+       self._generate_graph_per_generation(g, inds, groups, saved=saved, show=show)
+
+  def generate_stats_generations(self, ftype="avg", saved=True, show=False):
+    gen = self.logbook.select("generations")
+    all_inds = self.logbook.chapters["individual_fitness"].select(ftype)
+    all_groups = self.logbook.chapters["group_fitness"].select(ftype)
+    graphs.generate_graph_all_data(gen, all_inds, all_groups, saved, show, self.personal_path, ftype)
+
+  def _generate_graph_per_generation(self, gen, inds, groups, saved=True, show=False):
+    graphs.generate_graph_weight_population(gen, inds, groups, saved, show, self.personal_path)
+
 
 def open_logboox(path):
   s = Stats("%s/logbook.pkl" % path)
