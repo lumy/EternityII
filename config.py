7c38b4b45ae729aebf96d74ea38b4ef5cce3a29c
Adrien Vigouroux <adrien.vigour@gmail.com>
2017-02-03 16:35:48 +0100

    #3: Config file edit
    Config file edit

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ IMG="./Eternity/"
 
 population_file_saved="try01.pickle"
 population_file_base="e2pieces.txt"
-NGEN = 200
+NGEN = 1
 NPOPULATION = 10
 
 
-mutate_inpd=0.4selection_ind_value=1
+mutate_inpd=0.4
+selection_ind_value=1
