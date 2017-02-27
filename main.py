f179843b3974ebb6ee2cbe0da0d60f9b26f7926b
lumy <lumy@lumy.me>
2017-02-27 19:49:42 +0100

    #61 documentation updated in the main

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 """
   Main Module
 
+
+  main file can be called as ```python main.py --help```
+  Or ```python main.py -l [num_loop] -t [min_loop] --old-pop --timed```
+
+  ```--loop|-l``` Number of loop maximum to do. if set to -1 it will not be \
+  used to stop the loop(default: config.NGEN).
+  ```--time|-t``` Maximum time to execute the loop in min, if not set will not \
+  be used to stop the loop (default: None)
+  ```--timed``` iteration and loop would be timed. (not really usefull).
+  ```--old-pop|-o``` Load an old population. path is set in config file at \
+  config.population_file_saved.
 """
 import argparse
 import os
@@ -17,33 +28,37 @@ os.environ['TCL_LIBRARY'] = "C:\\Python27\\tcl\\tcl8.5"
 os.environ['TK_LIBRARY'] = "C:\\Python27\\tcl\\tk8.5"
 
 
-def _load_file(s):
+def _load_file(path):
   """
-    _load_file
-  :param s:
-  :return:
+    Use to load a Puzzle with dill.
+
+  :param str path: path to the file to load
+  :return: object (excepted [Puzzle](doc/puzzle.md) but no check is made)
   """
   try:
-    with open(s, "rb") as e:
+    with open(path, "rb") as e:
       puzzle.Puzzle.dynamique_type()
       return dill.load(e)
   except Exception as e:
-    print "Exception while loading file %s" % s
+    print "Exception while loading file %s" % path
     print e
     return None
 
 
 def load_population(old_pop=False):
   """
+    Load an old population or a new one. If old_pop is False, the new \
+    population will be loaded from config.population_file_base.
 
-  :return:
+  :param bool old_pop: loading the old population saved at \
+  config.population_file_saved
+  :return: A [Puzzle](doc/puzzle.md) object.
   """
   if old_pop:
     f = _load_file(config.population_file_saved)
     if f != None:
       return f
 
-  # Loading a basic Population with a runner
   inds = ind.get_population()
   corner = [i for i in inds if i[1].count(0) == 2]
   border = [i for i in inds if i[1].count(0) == 1]
@@ -53,9 +68,11 @@ def load_population(old_pop=False):
 
 def save_population(puzzle):
   """
-  Should we be using personal path ?
-  :param puzzle:
-  :return:
+    Save a given puzzle into the path config.population_file_saved.
+
+  :param [puzzle](doc/puzzle.md): A [Puzzle](doc/puzzle.md) object.
+  :return: None
+  :throw Exception: Can throw classic exception around open and dill.dump
   """
   with open(config.population_file_saved, "wb") as f:
     dill.dump(puzzle, f)
@@ -63,11 +80,22 @@ def save_population(puzzle):
 
 def one_turn(puzzle, generation, write_stats):
   """
+    Represent One iteration of the Algorithm.
+    Ex.
 
-  :param puzzle:
-  :param generation:
-  :param write_stats:
-  :return:
+    1. select
+    2. crossover
+    3. mutate
+    4. evaluate
+    5. log_stats
+
+    More information at [Current Algorithm](doc/Algorithm.md)
+
+  :param [puzzle](doc/puzzle.md): A [Puzzle](doc/puzzle.md) object.
+  :param int generation: Iteration index.
+  :param bool write_stats: Should we be logging stats. Used during benchmark, \
+  otherwise always set to True.
+  :return bool: solution as been found or Not.
   """
   # Example of call
   last_con = puzzle.stats.logbook.select("connections_completions")[-1]
@@ -90,12 +118,18 @@ def one_turn(puzzle, generation, write_stats):
 
 def loop(puzzle, write_stats, nloop=None, timer=None):
   """
+    This function loop with stopping conditions as set in params. Write the \
+    logbook at the end of the run.
 
-  :param puzzle:
-  :param write_stats:
-  :param nloop: argparse set nloop to config.ngen if not set.
-  :param time:
-  :return:
+  :param [puzzle](doc/puzzle.md): A [puzzle](doc/puzzle.md) object.
+  :param bool write_stats: Should we be logging stats. Used during benchmark, \
+  otherwise always set to True.
+  :param int nloop: Number of loop to do, if -1 will not be used to stop the \
+  loop.
+  :param float timer: Minute to turn the loop if None will not be used to stop \
+  loop.
+
+  :return bool: is the solution has been found or not
   """
   end_time = None
   iteration = 0
@@ -105,9 +139,7 @@ def loop(puzzle, write_stats, nloop=None, timer=None):
   while (nloop == -1 or iteration < nloop) and (end_time is None or time.time() < end_time):
 
     if one_turn(puzzle, iteration, write_stats):
-      print "Solution Found !"
       if write_stats:
-        # Saving logbook
         puzzle.write_stats()
       return True
     if iteration % 500 == 0 and iteration != 0:
@@ -115,22 +147,23 @@ def loop(puzzle, write_stats, nloop=None, timer=None):
       puzzle.stats.free_memory()
     iteration += 1
 
-  print "No Solution Look at the logbook."
   if write_stats:
     puzzle.write_stats()
     save_population(puzzle)
-
+  return False
 
 def main(write_stats, old_pop=False, timer=None, nloop=None, timed=False):
   """
-    main function will load a new population or an old one and run it.
+    main function will load a new population or an old one and run it with our \
+    [Current Algorithm](doc/Algorithm.md)
 
-  :param write_stats:
-  :param old_pop:
-  :param timer:
-  :param nloop:
+   :param bool write_stats: Should we be logging stats. Used during benchmark, \
+  otherwise always set to True.
+  :param bool old_pop: loading the old population saved at \
+  config.population_file_saved
+  :param float timer: see [loop](doc/main.md#loop)
+  :param int nloop: see [loop](doc/main.md#loop)
   :param timed: that will activate some timer, to calculate how many time for one iteration and for the whole iteration.
-  :return:
   """
   try:
     os.mkdir("./gen/")
@@ -145,8 +178,9 @@ def main(write_stats, old_pop=False, timer=None, nloop=None, timed=False):
 
 def get_args():
   """
+    Function to Set and Parse args with argparse.
 
-  :return:
+  :return: object
   """
   help="""Run a population with arguments and config file.
   """
@@ -168,7 +202,7 @@ if __name__ == '__main__':
        timer=None if kwargs.time is None else float(kwargs.time),
        nloop=int(kwargs.loop))
 
-__all__ = [ # Order of appearance in the documentation
+__md__ = [ # Order of appearance in the documentation
   'main',
   'loop',
   'one_turn',
@@ -176,4 +210,9 @@ __all__ = [ # Order of appearance in the documentation
   'save_population',
   'load_population',
   '_load_file'
+]
+__all__ = [
+  'main',
+  'loop',
+  'one_turn',
 ]
