cc2cb3a5c8dc76f8bed5b49dde344e2f98d00265
Lumy <lumy@lumy.me>
2018-04-25 15:55:06 +0200

    \#restart few changes operated in ind. added doc.

--- doc/ind.md
+++ doc/ind.md
@@ -1,41 +1,60 @@
-# ind
+## ind
 
-Use to represent an individual a 4 int direction and a rotation. (and an uid to keep the line reference)
+ - Load all tils.
 
+ - Ind objet is the representation of a unique tile. See [Ind](#Ind) description.
 
+A schema is an int representing a color/schema for a til.
 
+### mask-description
+      
+A mask is a list of None (No-Color specify) or a schema (0 is border: grey)
 
-# Class
-## Ind(self, f, l)
-
-The Individual reorganize the line in CLOCKWORK MOD (orange ?) so it can easily rotate them.
-
-
-- uid: Represant the id of the tile for final pr[int](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-[float](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex)-long-complex)ing
-- line: Line in order North South Weast East.
-
-### \_mask_(self, mask, c_index=0)
-
-return True if the mask is ok with the content
-
-- mask: [list](https://docs.python.org/2/tutorial/datastructures.html#more-on-lists) of None and 0
-
-- return:
+eg: [None, None, 1, 1]
 
 
 
-### best_value_of_mask(self, mask)
+## Class
+### Ind(self, func, lines)
+
+Ind class represent a til:
+
+- Ind.uid unique id for schema repr
+- Ind.content list int [schema:int, schema:int, schema:int, schema:int] directions in order North Est South Weast
+- Ind.rotation number of clockwork rotation apploied (range 0, 3)
 
 
-- mask:
 
-- return:
+The func, lines args are actually gonna be change, for an iterator or for simple uid,content, rotation arguments.
+The idea was to not initialize on the file but on randomized lines.
+
+
+- func: function that take lines in params and return one line -> uid, [North-schema South-schema Weast-schema East-schema].
+- lines: Lines|arg to be given to func
+
+#### \_mask_(self, mask, c_index=0)
+
+
+- mask: [list](https://docs.python.org/2/tutorial/datastructures.html#more-on-lists) of None and 0.
+- c_index: See [Mask](#mask)
+
+- return: value of fitness [0-4].
 
 
 
-### count(self, obj)
+#### best_value_of_mask(self, mask)
 
-Use to count how many occurencences of the color in the current ind
+Find the best value possible for a mask.
+
+- mask: mask to test against.
+
+- return: value between 0 and 4 that represant individual connection/score.
+
+
+
+#### count(self, obj)
+
+Use to count how many occurencences of the schema in the current ind
 
 - obj:
 
@@ -43,36 +62,40 @@ Use to count how many occurencences of the color in the current ind
 
 
 
-### mask(self, mask, c_index=0)
+#### mask(self, mask, c_index=0)
+
+test a [Mask](#mask-description)
+
+- mask: [Mask](#mask-description)
+- c_index: index content to start at, this simulate rotation.
+
+- return: True if the mask fit.
+
+
+
+#### rotate(self)
+
+Rotate the ind.
+
+
+
+
+#### rotates(self, nb)
+
+
+- nb: rotates nb times.
 
 
 
 
 
-
-### rotate(self)
-
+## Functions
 
 
-
-
-
-### rotates(self, nb)
-
-
-
-
-
-
-
-
-# Functions
-
-
-### get_population()
+#### get_population()
 
 Load the basic Population from the file e2pieces.txt
 
 
-- return: a [list](https://docs.python.org/2/tutorial/datastructures.html#more-on-lists) of [int](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-[float](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex)-long-complex)
+- return: a [list](https://docs.python.org/2/tutorial/datastructures.html#more-on-lists) of (UID:[int](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-[float](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex)-long-complex), [schema:[int](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-[float](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex)-long-complex), schema:[int](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-[float](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex)-long-complex), schema:[int](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-[float](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex)-long-complex), schema:[int](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-[float](https://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex)-long-complex)]
 
--- ind.py
+++ ind.py
@@ -1,56 +1,81 @@
 """
-Use to represent an individual a 4 int direction and a rotation. (and an uid to keep the line reference)
-"""
-import copy
+ - Load all tils.
+ - Ind objet is the representation of a unique tile. See [Ind](#Ind) description.
 
+A schema is an int representing a color/schema for a til.
+
+### mask-description
+      
+A mask is a list of None (No-Color specify) or a schema (0 is border: grey)
+
+eg: [None, None, 1, 1]
+"""
+#!/usr/bin/env python
+import copy
 import config
 
 
 def get_population():
   """
   Load the basic Population from the file e2pieces.txt
-  :return: a list of int
+  :return: a list of (UID:int, [schema:int, schema:int, schema:int, schema:int]
   """
   with open(config.population_file_base) as f:
     l = f.readlines()
   l = [(x + 1, [int(i) for i in l[x].split()]) for x in range(0, len(l))]
   return l
 
-class Ind(object):
-  def __init__(self, f, l):
-    """
-       The Individual reorganize the line in CLOCKWORK MOD (orange ?) so it can easily rotate them.
 
-    :param uid: Represant the id of the tile for final printing
-    :param line: Line in order North South Weast East.
+class Ind(object):
+  """
+
+       Ind class represent a til:
+
+         - Ind.uid unique id for schema repr
+         - Ind.content list int [schema:int, schema:int, schema:int, schema:int] directions in order North Est South Weast
+         - Ind.rotation number of clockwork rotation apploied (range 0, 3)
+  """
+
+  def __init__(self, func, lines):
     """
-    self.uid, dir = f(l)
-    n, s, w, e = dir
+    The func, lines args are actually gonna be change, for an iterator or for simple uid,content, rotation arguments.
+    The idea was to not initialize on the file but on randomized lines.
+
+    :param func: function that take lines in params and return one line -> uid, [North-schema South-schema Weast-schema East-schema].
+    :param lines: Lines|arg to be given to func
+    """
+    self.uid, dirs = func(lines)
+    n, s, e, w = dirs
     self.content = [n, e, s, w]
     self.rotation = 0
 
-  def __getitem__(self, item):
-    return copy.copy(self.content[item])
+  def __getitem__(self, index):
+    """
+      :return:  schema -> int
+    """
+    return self.content[index]
 
   def __setitem__(self, key, value):
-    self.content[key] = value
+    """ Shouldn't be used
+    """
+    raise NotImplemented("Not Authorized")
+#    self.content[key] = value
 
   def best_value_of_mask(self, mask):
     """
-
-    :param mask:
-    :return:
+    Find the best value possible for a mask.
+    :param mask: mask to test against.
+    :return: value between 0 and 4 that represant individual connection/score.
     """
     t = [self._mask_(mask, c_index=0), self._mask_(mask, c_index=1), self._mask_(mask, c_index=2),
          self._mask_(mask, c_index=3)]
-    # Return value between 0 and 4 that represant individual connection/score
     return t[t.index(max(t))]
 
   def _mask_(self, mask, c_index=0):
     """
-          return True if the mask is ok with the content
-        :param mask: list of None and 0
-        :return:
+        :param mask: list of None and 0.
+        :param c_index: See [Mask](#mask)
+        :return: value of fitness [0-4].
     """
     def _mask(m, index, c):
       if c < 4:
@@ -65,25 +90,37 @@ class Ind(object):
     return _mask(mask, c_index, 0)
 
   def mask(self, mask, c_index=0):
+    """
+      test a [Mask](#mask-description)
+      :param mask: [Mask](#mask-description)
+      :param c_index: index content to start at, this simulate rotation.
+      :return: True if the mask fit.
+    """
     if 4 == self._mask_(mask, c_index=c_index):
       return True
     return False
 
   def count(self, obj):
     """
-        Use to count how many occurencences of the color in the current ind
+        Use to count how many occurencences of the schema in the current ind
     :param obj:
     :return:
     """
     return len([x for x in self.content if x == obj])
 
   def rotate(self):
+    """
+      Rotate the ind.
+    """
     self.content = self.content[1:] + [self.content[0]]
     self.rotation += 1
     if self.rotation >= 4:
       self.rotation = 0
 
   def rotates(self, nb):
+    """
+    :param nb: rotates nb times.
+    """
     while nb > 0:
       nb -= 1
       self.rotate()
@@ -102,11 +139,12 @@ __all__ = [
   ]
 
 if __name__ == '__main__':
+  # should have test / test_ind.py
   i = Ind(lambda x: (0, [1, 0, 0, 17]), None)
-
-  print i.mask([0, 0, None, None])
+  mask = [0, 0, None, None]
+  assert i.mask(mask) == False, "shouldn't fit %s %s" % (i, mask)
   i.rotate()
-  print i.mask([0, 0, None, None])
+  assert i.mask(mask) == True, "should fit %s %s" % (i, mask)
   i.rotate()
-  print i.mask([0, 0, None, None])
-  print i
+  assert i.mask(mask) == False, "shouldn't fit %s %s" % (i, mask)
+  get_population()
