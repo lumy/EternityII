14a37ff5fd79d44777245217b9d0dc1609f4018d
Lumy <lumy@lumy.me>
2018-05-06 16:48:52 +0200

    web  configuration file

---
+++ web/config.py
@@ -0,0 +1,10 @@
+
+class Limit():
+  NGEN = (1, 2000, int, "limit for ngen is 1 to 2000.")
+  mutate_inpd = (0.001, 1.0, float, "limit for mutate_inpd is 0.001 to 1.0")
+  selection_ind_value_step = (1, 100, int, "TODO")
+  elitism_percentage_start = (1.0, 100.0, float, "TODO")
+  elitism_percentage_up = (1.0, 100.0, float, "TODO")
+  gen_modulo_elitism = (1.0, 100.0, float, "TODO")
+  select_light = (1.0, 100.0, float, "TODO")
+  select_medium = (1.0, 100.0, float, "TODO")
